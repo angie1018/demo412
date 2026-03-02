9e4fca17ba17f72a3cc8fedfe79daecd6f26de30
Angie <angiesofiahh@gmail.com>
2026-03-02 08:02:56 -0500

    Creacion de nuevas clases

---
+++ src/main/java/com/example/Docente.java
@@ -0,0 +1,9 @@
+package com.example;
+
+public class Docente  extends Persona {
+   
+    Docente() {
+        System.out.println("Constructor vacío de la clase docente");
+    }
+    
+}
---
+++ src/main/java/com/example/Estudiante.java
@@ -0,0 +1,9 @@
+package com.example;
+
+public class Estudiante extends Persona {
+
+    Estudiante() {
+        System.out.println("Constructor vacío de la clase estudiante");
+    }
+    
+}
