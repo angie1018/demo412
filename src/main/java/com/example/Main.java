6a724cdcaead97a91249fdf5b9a71f68a789565f
Angie <angiesofiahh@gmail.com>
2026-03-02 07:44:06 -0500

    Step 3: Upgrade Java Compiler Properties - Update maven.compiler source/target from 17 to 21

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -11,4 +11,23 @@ public class Main {
 
     persona1.dormir(8);
     }
+
+    public int suma(int a, int b) {
+        return a + b;
+    }
+
+    public int resta(int a, int b) {
+        return a - b;
+    }
+
+    public int multiplicacion(int a, int b) {
+        return a * b;
+    }
+
+    public double division(int a, int b) {
+        if (b == 0) {
+            throw new IllegalArgumentException("Division by zero not allowed");
+        }
+        return (double) a / b;
+    }
 }
