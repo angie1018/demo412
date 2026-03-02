e3012fde3f553eaed7935265e5bf68c7abe38618
Angie <angiesofiahh@gmail.com>
2026-03-02 06:46:39 -0500

    Creando clase persona

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -2,26 +2,13 @@ package com.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-    
-    // Métodos para testear
-    public int suma(int a, int b) {
-        return a + b;
-    }
-    
-    public int resta(int a, int b) {
-        return a - b;
-    }
-    
-    public int multiplicacion(int a, int b) {
-        return a * b;
-    }
-    
-    public double division(int a, int b) {
-        if (b == 0) {
-            throw new IllegalArgumentException("No se puede dividir por cero");
-        }
-        return (double) a / b;
+  
+
+    Persona persona1 = new Persona(123456789, "Juan Pérez", "juan.perez@example.com", 300123456);
+
+    String saludo = persona1.hablar("Hola, ¿Cómo estás?");
+    System.out.println(saludo);
+
+    persona1.dormir(8);
     }
 }
---
+++ src/main/java/com/example/Persona.java
@@ -0,0 +1,47 @@
+package com.example;
+
+public class Persona {
+
+    //Atributos de la clase persona
+    //Características que identifican a una clase Persona
+
+    private  int cedula;
+    public String nombre;
+    public String correo;
+    private int telefono;
+
+    //Constructor  vacío
+    Persona(){
+        System.out.println("Constructor vacío de Persona ejecutado.");// inicializaciónd el constructor de la clase Persona
+    }
+
+    //Constructor de la clase Persona con parámtros
+    Persona(int cedula, String nombre, String correo, int telefono) {
+        System.out.println("Constructor con parámetros de Persona ejecutado.");// inicialización del constructor de la clase Persona
+        this.cedula = cedula;
+        this.nombre = nombre;
+        this.correo = correo;
+        this.telefono = telefono;
+    }
+
+    // Métodos de la clase Persona
+    // Comportamientos que una clase Persona puede realizar o acciones que puede ejecutar
+
+    public String hablar(String saludo) {
+        // System.out.println(saludo);
+        return saludo + " Bien gracias!";
+    }
+
+    public void dormir(int horas) {
+        System.out.println("La persona ha dormido " + horas + " horas.");
+    }
+
+    public void mostrarInformacion() {
+        System.out.println("Información de la persona:");
+        System.out.println("Cédula: " + cedula);
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Correo: " + correo);
+        System.out.println("Teléfono: " + telefono);
+    }
+
+}
